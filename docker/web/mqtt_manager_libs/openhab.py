9cf05f429bc5c2462360433f61d410274067f6de
Tim Panajott <vpklotar@gmail.com>
2023-06-08 23:25:35 +0200

    Changed ItemStateEvent to ItemStateChangedEvent for OpenHAB

--- docker/web/mqtt_manager_libs/openhab.py
+++ docker/web/mqtt_manager_libs/openhab.py
@@ -28,8 +28,9 @@ def init(settings_from_manager, mqtt_client_from_manager):
 
 def on_message(ws, message):
     json_msg = json.loads(message)
-    if json_msg["type"] == "ItemStateEvent":
-        logging.info(F"Received command payload: {message}")
+    # if json_msg["type"] == "ItemStateEvent":
+    if json_msg["type"] == "ItemStateChangedEvent":
+        # logging.info(F"Received command payload: {message}")
         topic_parts = json_msg["topic"].split("/")
         item = topic_parts[2]
         payload = json.loads(json_msg["payload"])
