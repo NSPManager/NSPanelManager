12001d03b265a978b34a61acd1f04c29e7d74856
Tim Panajott <vpklotar@gmail.com>
2023-07-12 12:18:55 +0200

    Fixed run.sh so that it isn't as dependant on file permissions

--- docker/web/run.sh
+++ docker/web/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo "Starting mqtt_manager.py in background"
-./mqtt_manager.py &
+/usr/local/bin/python ./mqtt_manager.py &
 
 cd nspanelmanager
 
 echo "Running migrations..."
-./manage.py migrate
+/usr/local/bin/python ./manage.py migrate
 
 echo "Starting server"
-./manage.py runserver 0.0.0.0:8000
+/usr/local/bin/python ./manage.py runserver 0.0.0.0:8000
 
 echo "Stopping all python applications"
 for app in "$(pgrep python)"; do
