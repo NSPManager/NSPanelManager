d94340285d0314086a736f357973a02f91e2bc3a
Tim Panajott <vpklotar@gmail.com>
2023-07-12 12:25:12 +0200

    Fixed a bug where panel would not be populated with Home Assistant values and would crash uppon first start

--- docker/web/nspanelmanager/web/apps.py
+++ docker/web/nspanelmanager/web/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 import environ
+import logging
 
 
 class WebConfig(AppConfig):
@@ -7,17 +8,19 @@ class WebConfig(AppConfig):
     name = 'web'
 
     def ready(self):
-        environment = environ.Env()
-        if "IS_HOME_ASSISTANT_ADDON" in environment and environment("IS_HOME_ASSISTANT_ADDON") == "true":
-            if "SUPERVISOR_TOKEN" in environment:
-                from .settings_helper import get_setting_with_default, set_setting_value
-                if get_setting_with_default("home_assistant_token", "") == "" and get_setting_with_default("home_assistant_address", "") == "":
-                    print("No home assistant address or token stored, setting according to addon environment.")
-                    set_setting_value("home_assistant_token", environment("SUPERVISOR_TOKEN"))
-                    set_setting_value("home_assistant_address", "http://supervisor")
-                elif get_setting_with_default("home_assistant_token", "") != environment("SUPERVISOR_TOKEN"):
-                    print("Home Assistant token has changed. Will update database.")
-                    set_setting_value("home_assistant_token", environment("SUPERVISOR_TOKEN"))
-                #from .models import Settings
-                #objects = Settings.objects.filter(name=name)
-
+        try:
+            environment = environ.Env()
+            if "IS_HOME_ASSISTANT_ADDON" in environment and environment("IS_HOME_ASSISTANT_ADDON") == "true":
+                if "SUPERVISOR_TOKEN" in environment:
+                    from .settings_helper import get_setting_with_default, set_setting_value
+                    if get_setting_with_default("home_assistant_token", "") == "" and get_setting_with_default("home_assistant_address", "") == "":
+                        print("No home assistant address or token stored, setting according to addon environment.")
+                        set_setting_value("home_assistant_token", environment("SUPERVISOR_TOKEN"))
+                        set_setting_value("home_assistant_address", "http://supervisor")
+                    elif get_setting_with_default("home_assistant_token", "") != environment("SUPERVISOR_TOKEN"):
+                        print("Home Assistant token has changed. Will update database.")
+                        set_setting_value("home_assistant_token", environment("SUPERVISOR_TOKEN"))
+                    #from .models import Settings
+                    #objects = Settings.objects.filter(name=name)
+        except:
+            logging.exception("Failed to populate Home Assistant addon settings.")
