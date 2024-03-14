8e3dffa37bd6c24b0f26893b4844fd1b8eca311f
Tim Panajott <vpklotar@gmail.com>
2024-03-14 20:33:23 +0100

    Fixed issue #136 and also implemented drag and drop feature

--- docker/web/nspanelmanager/web/static/base.js
+++ docker/web/nspanelmanager/web/static/base.js
@@ -40,7 +40,61 @@ $(document).ready(function () {
       if(_last_dropdown_id_activated != "") {
         $("#" + _last_dropdown_id_activated).addClass("hidden");
       }
-     });
+    });
+
+    // Setup filename display when uploading files
+    $("#firmware").change(() => {
+      var filename = $('#firmware').val().replace(/C:\\fakepath\\/i, '')
+      $("#save_new_firmware_modal_subtext").html("Selected file: '" + filename + "'.");
+    });
+
+    $("#data_upload_file_name").change(() => {
+      var filename = $('#data_upload_file_name').val().replace(/C:\\fakepath\\/i, '')
+      $("#save_new_data_file_modal_subtext").html("Selected file: '" + filename + "'.");
+    });
+
+    $("#tft_file").change(() => {
+      var filename = $('#tft_file').val().replace(/C:\\fakepath\\/i, '')
+      $("#save_new_tft_file_modal_subtext").html("Selected file: '" + filename + "'.");
+    });
+
+    // Setup drag and drop feature
+    $('#firmware_dragdrop_target').on('dragover', (e) => {e.preventDefault(); e.stopPropagation();});
+    $('#firmware_dragdrop_target').on('dragenter', (e) => {e.preventDefault(); e.stopPropagation();});
+    $('#data_file_dragdrop_target').on('dragover', (e) => {e.preventDefault(); e.stopPropagation();});
+    $('#data_file_dragdrop_target').on('dragenter', (e) => {e.preventDefault(); e.stopPropagation();});
+    $('#tft_dragdrop_target').on('dragover', (e) => {e.preventDefault(); e.stopPropagation();});
+    $('#tft_dragdrop_target').on('dragenter', (e) => {e.preventDefault(); e.stopPropagation();});
+
+    $("#firmware_dragdrop_target").on('drop', (e) => {
+      if(e.originalEvent.dataTransfer && e.originalEvent.dataTransfer.files.length) {
+        e.preventDefault();
+        e.stopPropagation();
+        $("#firmware").prop("files", e.originalEvent.dataTransfer.files);
+        var filename = $('#firmware').val().replace(/C:\\fakepath\\/i, '')
+        $("#save_new_firmware_modal_subtext").html("Selected file: '" + filename + "'.");
+      }
+    });
+
+    $("#data_file_dragdrop_target").on('drop', (e) => {
+      if(e.originalEvent.dataTransfer && e.originalEvent.dataTransfer.files.length) {
+        e.preventDefault();
+        e.stopPropagation();
+        $("#data_upload_file_name").prop("files", e.originalEvent.dataTransfer.files);
+        var filename = $('#data_upload_file_name').val().replace(/C:\\fakepath\\/i, '')
+        $("#save_new_data_file_modal_subtext").html("Selected file: '" + filename + "'.");
+      }
+    });
+
+    $("#tft_dragdrop_target").on('drop', (e) => {
+      if(e.originalEvent.dataTransfer && e.originalEvent.dataTransfer.files.length) {
+        e.preventDefault();
+        e.stopPropagation();
+        $("#save_new_tft_file_modal_subtext").prop("files", e.originalEvent.dataTransfer.files);
+        var filename = $('#tft_file').val().replace(/C:\\fakepath\\/i, '')
+        $("#save_new_tft_file_modal_subtext").html("Selected file: '" + filename + "'.");
+      }
+    });
 });
 
 function addNewRoom() {
