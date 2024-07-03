af1863ca50906e12f4a8374d7dde14fb35041553
Tim Panajott <vpklotar@gmail.com>
2024-07-03 23:31:13 +0200

    Will now load assumed target platform architecture from dpkg-archtecture instead of assuming linux/amd64

--- docker/MQTTManager/compile_mqttmanager.sh
+++ docker/MQTTManager/compile_mqttmanager.sh
@@ -5,25 +5,25 @@ TARGETPLATFORM=""
 STRIP=""
 
 while true; do
-	case "$1" in
-	--target-platform)
-		TARGETPLATFORM="$2"
-		echo "Will compile for platform $TARGETPLATFORM"
-		shift # Remove --target-platform
-		shift # Remove value for target platform
-		;;
-	--strip)
-		echo "Will strip compiled binaries and .so's"
-		STRIP="1"
-		shift
-		;;
-	*) break ;;
-	esac
+  case "$1" in
+  --target-platform)
+    TARGETPLATFORM="$2"
+    echo "Will compile for platform $TARGETPLATFORM"
+    shift # Remove --target-platform
+    shift # Remove value for target platform
+    ;;
+  --strip)
+    echo "Will strip compiled binaries and .so's"
+    STRIP="1"
+    shift
+    ;;
+  *) break ;;
+  esac
 done
 
 if [ -z "$TARGETPLATFORM" ]; then
-	echo "No platform given as argument, will assume linux/amd64"
-	TARGETPLATFORM="linux/amd64"
+  TARGETPLATFORM="linux/$(dpkg-architecture -q DEB_BUILD_ARCH_CPU)"
+  echo "No platform given as argument, will assume $TARGETPLATFORM"
 fi
 
 echo "Compiling for target '$TARGETPLATFORM'"
@@ -35,61 +35,61 @@ deb_add_arch=""
 strip_bin=""
 cp /root/.conan2/profiles/default /root/.conan2/profiles/host
 if [ "$TARGETPLATFORM" == "linux/386" ]; then
-	deb_add_arch="i386"
-	conan_target_arch="x86"
-	strip_bin="/usr/bin/strip"
-	apt -y install gcc-multilib g++-multilib
+  deb_add_arch="i386"
+  conan_target_arch="x86"
+  strip_bin="/usr/bin/strip"
+  apt -y install gcc-multilib g++-multilib
 elif [ "$TARGETPLATFORM" == "linux/amd64" ]; then
-	deb_add_arch="amd64"
-	conan_target_arch="x86_64"
-	strip_bin="/usr/bin/strip"
+  deb_add_arch="amd64"
+  conan_target_arch="x86_64"
+  strip_bin="/usr/bin/strip"
 elif [ "$TARGETPLATFORM" == "linux/arm/v6" ]; then
-	deb_add_arch="armhf"
-	conan_target_arch="armv6"
-	strip_bin="/usr/bin/arm-linux-gnueabihf-strip"
-	apt -y install binutils-arm-linux-gnueabihf gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
-	echo "" >>/root/.conan2/profiles/host
-	echo "[buildenv]" >>/root/.conan2/profiles/host
-	echo "CC=arm-linux-gnueabihf-gcc-12" >>/root/.conan2/profiles/host
-	echo "CXX=arm-linux-gnueabihf-g++-12" >>/root/.conan2/profiles/host
-	echo "LD=arm-linux-gnueabihf-ld" >>/root/.conan2/profiles/host
+  deb_add_arch="armhf"
+  conan_target_arch="armv6"
+  strip_bin="/usr/bin/arm-linux-gnueabihf-strip"
+  apt -y install binutils-arm-linux-gnueabihf gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
+  echo "" >>/root/.conan2/profiles/host
+  echo "[buildenv]" >>/root/.conan2/profiles/host
+  echo "CC=arm-linux-gnueabihf-gcc-12" >>/root/.conan2/profiles/host
+  echo "CXX=arm-linux-gnueabihf-g++-12" >>/root/.conan2/profiles/host
+  echo "LD=arm-linux-gnueabihf-ld" >>/root/.conan2/profiles/host
 elif [ "$TARGETPLATFORM" == "linux/arm/v7" ]; then
-	deb_add_arch="armhf"
-	conan_target_arch="armv7hf"
-	strip_bin="/usr/bin/arm-linux-gnueabihf-strip"
-	apt -y install binutils-arm-linux-gnueabihf gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
-	echo "" >>/root/.conan2/profiles/host
-	echo "[buildenv]" >>/root/.conan2/profiles/host
-	echo "CC=arm-linux-gnueabihf-gcc-12" >>/root/.conan2/profiles/host
-	echo "CXX=arm-linux-gnueabihf-g++-12" >>/root/.conan2/profiles/host
-	echo "LD=arm-linux-gnueabihf-ld" >>/root/.conan2/profiles/host
+  deb_add_arch="armhf"
+  conan_target_arch="armv7hf"
+  strip_bin="/usr/bin/arm-linux-gnueabihf-strip"
+  apt -y install binutils-arm-linux-gnueabihf gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
+  echo "" >>/root/.conan2/profiles/host
+  echo "[buildenv]" >>/root/.conan2/profiles/host
+  echo "CC=arm-linux-gnueabihf-gcc-12" >>/root/.conan2/profiles/host
+  echo "CXX=arm-linux-gnueabihf-g++-12" >>/root/.conan2/profiles/host
+  echo "LD=arm-linux-gnueabihf-ld" >>/root/.conan2/profiles/host
 elif [ "$TARGETPLATFORM" == "linux/arm64" ]; then
-	deb_add_arch="arm64"
-	conan_target_arch="armv8"
-	strip_bin="/usr/bin/aarch64-linux-gnu-strip"
-	apt -y install binutils-aarch64-linux-gnu gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
-	echo "" >>/root/.conan2/profiles/host
-	echo "[buildenv]" >>/root/.conan2/profiles/host
-	echo "CC=aarch64-linux-gnu-gcc-12" >>/root/.conan2/profiles/host
-	echo "CXX=aarch64-linux-gnu-g++-12" >>/root/.conan2/profiles/host
-	echo "LD=aarch64-linux-gnu-ld" >>/root/.conan2/profiles/host
-	#echo "CFLAGS=-march=armv8-a" >>/root/.conan2/profiles/host
-	#echo "CXXFLAGS=-march=armv8-a" >>/root/.conan2/profiles/host
+  deb_add_arch="arm64"
+  conan_target_arch="armv8"
+  strip_bin="/usr/bin/aarch64-linux-gnu-strip"
+  apt -y install binutils-aarch64-linux-gnu gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
+  echo "" >>/root/.conan2/profiles/host
+  echo "[buildenv]" >>/root/.conan2/profiles/host
+  echo "CC=aarch64-linux-gnu-gcc-12" >>/root/.conan2/profiles/host
+  echo "CXX=aarch64-linux-gnu-g++-12" >>/root/.conan2/profiles/host
+  echo "LD=aarch64-linux-gnu-ld" >>/root/.conan2/profiles/host
+  #echo "CFLAGS=-march=armv8-a" >>/root/.conan2/profiles/host
+  #echo "CXXFLAGS=-march=armv8-a" >>/root/.conan2/profiles/host
 else
-	echo "ERROR !Unknown target platform. Will exit."
-	exit 1
+  echo "ERROR !Unknown target platform. Will exit."
+  exit 1
 fi
 eval $(dpkg-architecture) # Load in what the builder machine is
 
 # Setup apt source file to be able to access both architectures
 if [ "$deb_add_arch" != "$DEB_HOST_ARCH" ]; then
-	if [ ! -f "/etc/apt/sources.list.d/debian.sources.bak" ]; then
-		cp /etc/apt/sources.list.d/debian.sources /etc/apt/sources.list.d/debian.sources.bak
-	fi
-	cp /etc/apt/sources.list.d/debian.sources.bak /etc/apt/sources.list.d/debian.sources
-	dpkg --add-architecture $deb_add_arch
-	sed -i "/Components.*/a arch=$DEB_HOST_ARCH,$deb_add_arch" /etc/apt/sources.list.d/debian.sources
-	apt-get update
+  if [ ! -f "/etc/apt/sources.list.d/debian.sources.bak" ]; then
+    cp /etc/apt/sources.list.d/debian.sources /etc/apt/sources.list.d/debian.sources.bak
+  fi
+  cp /etc/apt/sources.list.d/debian.sources.bak /etc/apt/sources.list.d/debian.sources
+  dpkg --add-architecture $deb_add_arch
+  sed -i "/Components.*/a arch=$DEB_HOST_ARCH,$deb_add_arch" /etc/apt/sources.list.d/debian.sources
+  apt-get update
 fi
 
 apt -y install libssl-dev:$deb_add_arch openssl:$deb_add_arch libcurl4-openssl-dev:$deb_add_arch
@@ -120,7 +120,7 @@ sed -i "s|/MQTTManager/|/home/tim/NSPanelManager/docker/MQTTManager/|g" compile_
 cp compile_commands.json ../
 
 if [ "$STRIP" == "1" ]; then
-	echo "Stripping binaries and .so files using '$strip_bin'..."
-	"$strip_bin" /MQTTManager/build/*.so
-	"$strip_bin" /MQTTManager/build/nspm_mqttmanager
+  echo "Stripping binaries and .so files using '$strip_bin'..."
+  "$strip_bin" /MQTTManager/build/*.so
+  "$strip_bin" /MQTTManager/build/nspm_mqttmanager
 fi
