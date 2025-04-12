1835b16e8336b102135cce6152a53cdd3481ea16
Tim Panajott <vpklotar@gmail.com>
2025-04-12 16:56:53 +0200

    Will now reload MQTT settings when reloading config

--- docker/MQTTManager/src/main.cpp
+++ docker/MQTTManager/src/main.cpp
@@ -38,6 +38,7 @@ void sigusr1_handler(int signal) {
     std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
     MqttManagerConfig::load();
+    MQTT_Manager::reload_config();
     HomeAssistantManager::reload_config();
     OpenhabManager::reload_config();
     EntityManager::load_entities();
