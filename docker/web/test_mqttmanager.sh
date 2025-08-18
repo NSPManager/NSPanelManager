cf634ab8103610b776bb634939a6c4bef3702489
Tim Panajott <vpklotar@gmail.com>
2025-08-18 21:45:48 +0200

    Added script to automatically compile and run unit tests in MQTTManager

--- docker/web/test_mqttmanager.sh
+++ docker/web/test_mqttmanager.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Calling compile_mqttmanager --test $@"
-/MQTTManager/compile_mqttmanager.sh --test $@ && /MQTTManager/build/nspm_mqttmanager
+/MQTTManager/compile_mqttmanager.sh --test $@ && echo "Running MQTTMAnager in unit test mode" && /MQTTManager/build/nspm_mqttmanager
