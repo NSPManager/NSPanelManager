NSPManager/NSPanelManager

commit 60a039594a7c0c28798ffd8b5c461f4b74a6b9f6

Fixed a bug where it failed to find detached entities

docker/MQTTManager/include/entity_manager/entity_manager.hpp
@@ -137,8 +137,12 @@ public:
     std::lock_guard<std::mutex> mutex_guard(EntityManager::_entities_mutex);
     auto rit = EntityManager::_entities.cbegin();
     while (rit != EntityManager::_entities.cend()) {
-      if ((type != MQTT_MANAGER_ENTITY_TYPE::ANY && (*rit)->get_type() == type) && (*rit)->get_id() == id) {
-        return std::static_pointer_cast<EntityClass>(*rit);
+      if ((*rit)->get_id() == id) {
+        if (type == MQTT_MANAGER_ENTITY_TYPE::ANY) {
+          return std::static_pointer_cast<EntityClass>(*rit);
+        } else if ((*rit)->get_type() == type && (*rit)->get_id() == id) {
+          return std::static_pointer_cast<EntityClass>(*rit);
+        }
       } else {
         ++rit;
       }

docker/MQTTManager/include/nspanel/nspanel.cpp
@@ -1341,9 +1341,9 @@ void NSPanel::command_callback(NSPanelMQTTManagerCommand &command) {
                 (*entity)->toggle();
               }
             } else
-              SPDLOG_ERROR("Tried to toggle detached light via panel but no light was was found with configured ID.");
+              SPDLOG_ERROR("Tried to toggle detached entity via panel but no entity was found with configured ID '{}'.", this->_settings.button1_detached_mode_entity_id.value());
           } else {
-            SPDLOG_ERROR("Tried to toggle detached light via panel but no light was configured for button.");
+            SPDLOG_ERROR("Tried to toggle detached entity via panel but no entity was configured for button.");
           }
           break;
         }
@@ -1370,9 +1370,9 @@ void NSPanel::command_callback(NSPanelMQTTManagerCommand &command) {
                 (*entity)->toggle();
               }
             } else
-              SPDLOG_ERROR("Tried to toggle detached light via panel but no light was was found with configured ID.");
+              SPDLOG_ERROR("Tried to toggle detached entity via panel but no entity was found with configured ID '{}'.", this->_settings.button2_detached_mode_entity_id.value());
           } else {
-            SPDLOG_ERROR("Tried to toggle detached light via panel but no light was configured for button.");
+            SPDLOG_ERROR("Tried to toggle detached entity via panel but no entity was configured for button.");
           }
           break;
         }

docker/MQTTManager/include/websocket_server/websocket_server.cpp
@@ -247,13 +247,13 @@ void WebsocketServer::_websocket_message_callback(std::shared_ptr<ix::Connection
 
             std::lock_guard<std::mutex> lock_guard_callbacks(WebsocketServer::_on_stomp_send_message_callbacks_mutex);
             WebsocketServer::_on_global_stomp_send_message_callbacks(*frame);
-            if (WebsocketServer::_on_stomp_send_message_callbacks.count(frame->headers["destination"]) > 0) {
+            if (WebsocketServer::_on_stomp_send_message_callbacks.count(frame->headers["destination"]) > 0) [[likely]] {
               SPDLOG_DEBUG("Received STOMP SEND command for existing topic, setting topic '{}' to value '{}'", frame->headers["destination"], frame->body);
               WebsocketServer::_on_stomp_send_message_callbacks[frame->headers["destination"]](frame.value());
               return;
+            } else {
+              SPDLOG_WARN("Received STOMP SEND command for unknown topic. Will not publish, topic '{}', value '{}'", frame->headers["destination"], frame->body);
             }
-
-            SPDLOG_WARN("Received STOMP SEND command for unknown topic. Will not publish, topic '{}', value '{}'", frame->headers["destination"], frame->body);
           } else {
             SPDLOG_WARN("Received unknown STOMP frame type {}.", static_cast<int>(frame->type));
           }

commit 59dc9fb8ed4379caa2dc0f9322cc9f9cbb715e4b

Fixed a bug introduced by commit 60a039594a7c0c28798ffd8b5c461f4b74a6b9f6 that caused the manager to freeze when loading scenes

docker/MQTTManager/include/entity_manager/entity_manager.cpp
@@ -367,21 +367,23 @@ void EntityManager::load_scenes() {
     if (existing_scene) [[likely]] {
       (*existing_scene)->reload_config();
     } else {
-      std::lock_guard<std::mutex> mutex_guard(EntityManager::_entities_mutex);
 
       try {
         auto scene_settings = database_manager::database.get<database_manager::Scene>(scene_id);
         if (scene_settings.scene_type.compare("home_assistant") == 0) {
           std::shared_ptr<HomeAssistantScene> scene = std::shared_ptr<HomeAssistantScene>(new HomeAssistantScene(scene_settings.id));
           SPDLOG_INFO("Scene {}::{} was found in database but not in config. Creating scene.", scene->get_id(), scene->get_name());
+          std::lock_guard<std::mutex> mutex_guard(EntityManager::_entities_mutex);
           EntityManager::_entities.push_back(scene);
         } else if (scene_settings.scene_type.compare("openhab") == 0) {
           std::shared_ptr<OpenhabScene> scene = std::shared_ptr<OpenhabScene>(new OpenhabScene(scene_settings.id));
           SPDLOG_INFO("Scene {}::{} was found in database but not in config. Creating scene.", scene->get_id(), scene->get_name());
+          std::lock_guard<std::mutex> mutex_guard(EntityManager::_entities_mutex);
           EntityManager::_entities.push_back(scene);
         } else if (scene_settings.scene_type.compare("nspm_scene") == 0) {
           std::shared_ptr<NSPMScene> scene = std::shared_ptr<NSPMScene>(new NSPMScene(scene_settings.id));
           SPDLOG_INFO("Scene {}::{} was found in database but not in config. Creating scene.", scene->get_id(), scene->get_name());
+          std::lock_guard<std::mutex> mutex_guard(EntityManager::_entities_mutex);
           EntityManager::_entities.push_back(scene);
         } else {
           SPDLOG_ERROR("Unknown scene type '{}'. Will ignore entity.", scene_settings.scene_type);

docker/MQTTManager/include/entity_manager/entity_manager.hpp
@@ -140,7 +140,7 @@ public:
       if ((*rit)->get_id() == id) {
         if (type == MQTT_MANAGER_ENTITY_TYPE::ANY) {
           return std::static_pointer_cast<EntityClass>(*rit);
-        } else if ((*rit)->get_type() == type && (*rit)->get_id() == id) {
+        } else if ((*rit)->get_type() == type) {
           return std::static_pointer_cast<EntityClass>(*rit);
         }
       } else {

docker/MQTTManager/include/openhab_manager/openhab_manager.hpp
@@ -38,6 +38,11 @@ public:
 
     std::string data;
     data = OpenhabManager::_fetch_item_state_via_rest(item);
+    if (data.empty()) {
+      SPDLOG_WARN("Failed to fetch item state for '{}'. Will not be able to update items current state.", item);
+      return;
+    }
+
     nlohmann::json update_data;
     update_data["type"] = "ItemStateFetched";
     try {